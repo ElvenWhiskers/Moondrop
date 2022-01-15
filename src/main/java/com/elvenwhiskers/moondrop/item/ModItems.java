78af80912ce805ddb04eaa25a64a33d89efe2f23
Nina <Bringel.Brittany@Gmail.com>
2022-01-14 23:52:32 -0800

    Added Aegis ingot and Nugget

---
+++ src/main/java/com/elvenwhiskers/moondrop/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.elvenwhiskers.moondrop.item;
+
+import com.elvenwhiskers.moondrop.MoonDrop;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MoonDrop.MOD_ID);
+
+    public static final RegistryObject<Item> AEGIS_INGOT = ITEMS.register("aegis_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> AEGIS_NUGGET = ITEMS.register("aegis_nugget",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
