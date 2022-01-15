b62ac9087bbb983c766213e26476f11f4ab54353
Nina <Bringel.Brittany@Gmail.com>
2022-01-15 11:21:40 -0800

    Added pink starfruit logs and planks and flammables

--- src/main/java/com/elvenwhiskers/moondrop/block/ModBlocks.java
+++ src/main/java/com/elvenwhiskers/moondrop/block/ModBlocks.java
@@ -1,13 +1,19 @@
 package com.elvenwhiskers.moondrop.block;
 
 import com.elvenwhiskers.moondrop.MoonDrop;
+import com.elvenwhiskers.moondrop.block.custom.ModFlammableRotatedPillarBlock;
 import com.elvenwhiskers.moondrop.item.ModCreativeModeTab;
 import com.elvenwhiskers.moondrop.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -41,6 +47,35 @@ public class ModBlocks {
                     .strength(5f)
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.MOONDROP_TAB);
 
+    public static final RegistryObject<Block> PINK_STARFRUIT_LOG = registerBlock("pink_starfruit_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)), ModCreativeModeTab.MOONDROP_TAB);
+
+    public static final RegistryObject<Block> PINK_STARFRUIT_WOOD = registerBlock("pink_starfruit_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)), ModCreativeModeTab.MOONDROP_TAB);
+
+    public static final RegistryObject<Block> STRIPPED_PINK_STARFRUIT_LOG = registerBlock("stripped_pink_starfruit_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)), ModCreativeModeTab.MOONDROP_TAB);
+
+    public static final RegistryObject<Block> STRIPPED_PINK_STARFRUIT_WOOD = registerBlock("stripped_pink_starfruit_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)), ModCreativeModeTab.MOONDROP_TAB);
+
+    public static final RegistryObject<Block> PINK_STARFRUIT_PLANKS = registerBlock("pink_starfruit_planks",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)){
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return true;
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return 20;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return 5;
+                }
+            }, ModCreativeModeTab.MOONDROP_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
