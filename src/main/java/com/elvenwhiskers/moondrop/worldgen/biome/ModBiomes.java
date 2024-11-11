080ac4e4414d914040900f6db1734b94bd838404
Nina <bringel.brittany@gmail.com>
2024-11-11 13:15:06 -0800

    Added some basic update to biomes and dimension, though much more work is needed

---
+++ src/main/java/com/elvenwhiskers/moondrop/worldgen/biome/ModBiomes.java
@@ -0,0 +1,24 @@
+package com.elvenwhiskers.moondrop.worldgen.biome;
+
+import com.elvenwhiskers.moondrop.Moondrop;
+import com.elvenwhiskers.moondrop.worldgen.ModPlacedFeatures;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.Musics;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.*;
+import net.minecraft.world.level.levelgen.GenerationStep;
+
+public class ModBiomes {
+    //public static final ResourceKey<Biome> TEST_BIOME = register("test_biome");
+    //public static final ResourceKey<Biome> TEST_BIOME_2 = register("test_biome_2");
+
+    //private static ResourceKey<Biome> register(String name)
+    //{
+        //return ResourceKey.create(Registries.BIOME, new ResourceLocation(Moondrop.MODID, name));
+
+}
---
+++ src/main/java/com/elvenwhiskers/moondrop/worldgen/biome/ModTerraBlenderAPI.java
@@ -0,0 +1,11 @@
+package com.elvenwhiskers.moondrop.worldgen.biome;
+
+import com.elvenwhiskers.moondrop.Moondrop;
+import net.minecraft.resources.ResourceLocation;
+import terrablender.api.Regions;
+
+public class ModTerraBlenderAPI {
+    public static void registerRegions() {
+        //Regions.register(new ModOverworldRegion(new ResourceLocation(Moondrop.MODID, "overworld"), 5));
+    }
+}
