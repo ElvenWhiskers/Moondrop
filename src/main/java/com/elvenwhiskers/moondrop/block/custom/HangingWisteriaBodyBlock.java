e6c5651adf8bd14aa5d89866b4bcaf75394434a2
Nina <bringel.brittany@gmail.com>
2024-11-30 16:20:36 -0800

    hanging block cant grow on itself anymore

--- src/main/java/com/elvenwhiskers/moondrop/block/custom/HangingWisteriaBodyBlock.java
+++ src/main/java/com/elvenwhiskers/moondrop/block/custom/HangingWisteriaBodyBlock.java
@@ -2,9 +2,14 @@ package com.elvenwhiskers.moondrop.block.custom;
 
 import com.elvenwhiskers.moondrop.block.ModBlocks;
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class HangingWisteriaBodyBlock extends GrowingPlantBodyBlock {
@@ -21,6 +26,17 @@ public class HangingWisteriaBodyBlock extends GrowingPlantBodyBlock {
 
     protected GrowingPlantHeadBlock getHeadBlock() {
         return (GrowingPlantHeadBlock) ModBlocks.HANGING_BLUE_WISTERIA_VINES_HEAD.get();
+    }
+
+
+    //    @Override
+//    public BlockState updateShape(BlockState state, Direction direction, BlockState offsetState, LevelAccessor level, BlockPos pos, BlockPos offsetPos) {
+//        return direction == Direction.UP && !this.canSurvive(state, level, pos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, direction, offsetState, level, pos, offsetPos);
+//    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return Block.isFaceFull(level.getBlockState(pos.above()).getCollisionShape(level, pos.above()), Direction.DOWN) && !level.isWaterAt(pos);
     }
 }
 
--- src/main/java/com/elvenwhiskers/moondrop/block/custom/HangingWisteriaHeadBlock.java
+++ src/main/java/com/elvenwhiskers/moondrop/block/custom/HangingWisteriaHeadBlock.java
@@ -2,8 +2,11 @@ package com.elvenwhiskers.moondrop.block.custom;
 
 import com.elvenwhiskers.moondrop.block.ModBlocks;
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,8 +20,8 @@ public class HangingWisteriaHeadBlock extends GrowingPlantHeadBlock {
         return CODEC;
     }
 
-    public HangingWisteriaHeadBlock(BlockBehaviour.Properties p_154966_) {
-        super(p_154966_, Direction.DOWN, SHAPE, false, 0.1);
+    public HangingWisteriaHeadBlock(BlockBehaviour.Properties properties) {
+        super(properties, Direction.DOWN, SHAPE, false, 0.1);
     }
 
     protected int getBlocksToGrowWhenBonemealed(RandomSource p_222680_) {
@@ -31,5 +34,15 @@ public class HangingWisteriaHeadBlock extends GrowingPlantHeadBlock {
 
     protected boolean canGrowInto(BlockState p_154971_) {
         return NetherVines.isValidGrowthState(p_154971_);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState offsetState, LevelAccessor level, BlockPos pos, BlockPos offsetPos) {
+        return direction == Direction.UP && !this.canSurvive(state, level, pos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, direction, offsetState, level, pos, offsetPos);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return Block.isFaceFull(level.getBlockState(pos.above()).getCollisionShape(level, pos.above()), Direction.DOWN) && !level.isWaterAt(pos);
     }
 }
